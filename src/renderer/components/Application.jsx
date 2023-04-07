7148409b91be6d483e6bc2b16a1f261461c2d281
tcpip147 <tcpip147@gmail.com>
2023-04-07 15:29:58 +0900

    a

--- src/renderer/components/Application.jsx
+++ src/renderer/components/Application.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import './Application.scss';
+import _ from 'lodash';
 
 const Application = () => {
 
     return (
         <div className='application'>
-
+            {
+                <table>
+                    <thead>
+                        <tr>
+                            {
+                                _.range(0, 16).map((c) => {
+                                    return <td key={c}>{c}</td>;
+                                })
+                            }
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            _.range(0, 10).map((r) => {
+                                return (
+                                    <tr key={r}>
+                                        {
+                                            _.range(0, 16).map((c) => {
+                                                return <td key={c}>{r + "_" + c}</td>;
+                                            })
+                                        }
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table>
+            }
         </div>
     );
 };
